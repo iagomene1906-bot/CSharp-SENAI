205567a5f6ca6e8e60d8afbe007536d4f85486e5
FIC <fic@info.local>
2026-04-30 14:39:43 -0300

    :sparkles: feat: Exercícios da Aula06

--- Aula06/Exercícios/Program.cs
+++ Aula06/Exercícios/Program.cs
@@ -9,18 +9,29 @@ class Program
         // NIVÉL FÁCIL
 
         // EXERCÍCIO 1
+        // Enquanto c for menor ou igual a 10, adicione 1.
         // for(int c = 1; c <= 10; c++)
         // {
         //     Console.WriteLine(c);
         // }
 
         // EXERCÍCIO 2
+        // Enquanto c for maior ou igual a 1, subtraia 1. 
         // for(int c = 10; c >= 1; c--)
         // {
         //     Console.WriteLine(c);
         // }
 
         // EXERCÍCIO 3
+        // No for, fiz um laço para 1 ir somando + 1 até chegar a 10, depois peguei esta variável do for e fiz vezes n1, assim, finalizando a tabuada até o 10.
+        // int n1 = 7;
+
+        // for(int t = 1; t <= 10; t++)
+        // {
+        //     Console.WriteLine(n1 * t);
+        // }
+
+        // EXERCÍCIO 4
         
     }
 }
